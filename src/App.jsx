77b6f50229d4ac9f516dfb0bd80eba613cbdb3bd
Renato Oliveira <renato.loliveira89@gmail.com>
2024-03-06 16:31:29 -0300

    Criando página Animais

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Home from './pages/Home/Home';
+import Animais from './pages/Animais/Animais';
 
 function App() {
 
   return (
     <>
-      <Home />
+      <Animais />
     </>
   )
 }
---
+++ src/pages/Animais/Animais.jsx
@@ -0,0 +1,14 @@
+import Navegacao from '../../components/Navegacao/Navegacao';
+import CardAnimal from '../../components/CardAnimal/CardAnimal';
+import './Animais.css'
+
+function Animais() {
+    return(
+        <>
+            <Navegacao />
+            <CardAnimal />
+        </>
+    );
+}
+
+export default Animais
