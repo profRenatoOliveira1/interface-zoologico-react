874895ad44d78ef532defdaac93b6fa517eeb790
Renato Oliveira <renato.loliveira89@gmail.com>
2024-03-08 09:46:46 -0300

    Criando rotas para as páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Home from './pages/Home/Home';
 import Animais from './pages/Animais/Animais';
+import { BrowserRouter as Roteador, Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <Animais />
+      <Roteador>
+        <Routes>
+          <Route exact path='/' Component={Home} />
+          <Route path='/animais' Component={Animais} />
+        </Routes>
+      </Roteador>
     </>
   )
 }
--- src/components/Navegacao/Navegacao.jsx
+++ src/components/Navegacao/Navegacao.jsx
@@ -1,4 +1,5 @@
-import './Navegacao.css'
+import './Navegacao.css';
+import { Link } from 'react-router-dom';
 
 function Navegacao() {
 
@@ -10,11 +11,21 @@ function Navegacao() {
 
             <div className="navbar-opcoes">
                 <ul>
-                    <li>Início</li>
-                    <li>Os Animais</li>
-                    <li>Fotos</li>
-                    <li>Depoimentos</li>
-                    <li>Login</li>
+                    <li>
+                        <Link style={{color: "#A7A000"}} to="/">Início</Link>
+                    </li>
+                    <li>
+                        <Link style={{color: "#A7A000"}} to="/animais">Os Animais</Link>
+                    </li>
+                    <li>
+                        <Link style={{color: "#A7A000"}} to="/fotos">Fotos</Link>
+                    </li>
+                    <li>
+                        <Link style={{color: "#A7A000"}} to="/depoimentos">Depoimentos</Link>
+                    </li>
+                    <li>
+                        <Link style={{color: "#A7A000"}} to="/login">Login</Link>
+                    </li>
                 </ul>
             </div>
         </div>
