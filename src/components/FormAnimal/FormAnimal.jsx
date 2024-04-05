83a97c23fb13d723deeca40e56b29e32298b8b1a
Renato Oliveira <renato.loliveira89@gmail.com>
2024-04-05 16:31:19 -0300

    Inserindo o habitat no cadastro do animal

--- src/components/FormAnimal/FormAnimal.jsx
+++ src/components/FormAnimal/FormAnimal.jsx
@@ -1,13 +1,19 @@
 import './FormAnimal.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function FormAnimal() {
 
     const [formData, setFormData] = useState({
-        nomeAnimal: '',
-        generoAnimal: 'macho',
-        idadeAnimal: ''
+        nome: '',
+        genero: 'macho',
+        idade: '',
+        envergadura: '',
+        idHabitat: '1'
     });
+
+    // State para armazenar os habitats cadastrados no banco
+    const [habitats, setHabitats] = useState([]);
+    const [habitatAnimal, setHabitatAnimal] = useState(1);
 
     // Função para lidar com a mudança nos campos de entrada
     const handleChange = (e) => {
@@ -23,7 +29,7 @@ function FormAnimal() {
         e.preventDefault(); // Evita o recarregamento da página
         // Envia os dados do formulário para o servidor
         //console.log(JSON.stringify(formData));
-        fetch("http://localhost:3000/new/ave", {
+        fetch("http://localhost:3000/novo/ave", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -42,6 +48,43 @@ function FormAnimal() {
             });
     };
 
+    useEffect(() => {
+        const fetchHabitats = async () => {
+            try {
+                try {
+                    // Realiza uma requisição GET para o servidor
+                    const response = await fetch("http://localhost:3000/habitats");
+                    
+                    // Verifica se a requisição foi bem-sucedida
+                    if (!response.ok) {
+                      throw new Error('Erro ao buscar dados do servidor');
+                    }
+                    
+                    // Extrai os dados da resposta e os converte para JSON
+                    const jsonData = await response.json();
+                    
+                    // Retorna os dados recebidos do servidor
+                    setHabitats(jsonData);
+                  } catch (error) {
+                    console.error('Erro:', error);
+                    // Em vez de apenas logar o erro, você pode decidir como lidar com ele aqui
+                    throw error; // Lança o erro para que quem chame a função possa tratá-lo
+                  }
+            } catch (error) {
+                console.error('Erro ao buscar habitats:', error);
+            }
+        };
+
+        fetchHabitats();
+    }, []);
+
+    const atualizaHabitat = (e) => {
+        // Atualiza a variável habitatAnimal com o valor selecionado no select
+        const novoHabitat = e.target.value;
+        setHabitatAnimal(novoHabitat);
+        console.log(novoHabitat);
+    }
+
     return (
         <>
             <h2>Cadastro de Ave</h2>
@@ -50,8 +93,8 @@ function FormAnimal() {
                     <input
                         placeholder='Nome da ave'
                         type="text"
-                        name="nomeAnimal"
-                        value={formData.nomeAnimal}
+                        name="nome"
+                        value={formData.nome}
                         onChange={handleChange}
                     />
                 </label>
@@ -59,20 +102,41 @@ function FormAnimal() {
                     <input
                         placeholder='Idade'
                         type="number"
-                        name="idadeAnimal"
-                        value={formData.idadeAnimal}
+                        name="idade"
+                        value={formData.idade}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    <input
+                        placeholder='Envergadura'
+                        type="number"
+                        name="envergadura"
+                        value={formData.envergadura}
                         onChange={handleChange}
                     />
                 </label>
                 <label>
                     <select
-                        name="generoAnimal"
-                        value={formData.generoAnimal}
+                        name="genero"
+                        value={formData.genero}
                         onChange={handleChange}
                     >
                         <option value="macho">Macho</option>
                         <option value="femea">Fêmea</option>
                         <option value="desconhecido">Desconhecido</option>
+                    </select>
+                </label>
+                <label>
+                    <select
+                        onChange={atualizaHabitat}
+                    >
+                        {/* Verifica se o array de habitats está vazio, a função map só é chamada se o array tiver conteúdo */}
+                        {habitats && habitats.length > 0 && habitats.map(habitat => (
+                            <option key={habitat.idhabitat} value={habitat.idhabitat}>
+                                {habitat.nomehabitat}
+                            </option>
+                        ))}
                     </select>
                 </label>
                 <button type="submit">Enviar</button>
